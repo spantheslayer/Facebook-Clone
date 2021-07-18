11894237e5e4dc765823638baab4a47608bfe423
supriyo <supriyosarkar282@gmail.com>
2021-07-18 19:39:57 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Facebook Clone</title>
+      </Head>
+
+      <Header />
+
+      <main></main>
+    </div>
+  );
+}
